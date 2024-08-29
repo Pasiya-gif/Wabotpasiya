d473e34de657631f879d5cbca4b3a6eaeef67721
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-08-29 23:42:46 +0530

    Update Config.js

--- Config.js
+++ Config.js
@@ -7,6 +7,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://.avertars.githubusercontent.com/U/106251140?v=4".
+ALIVE_MSG: process.env.ALIVE_MSG ||  "Hello, I am yourName i am alive now!".
 };
 config.js
 Pasindu Madushan 
