fd53d4ebf99e0deded6cadf05e6a9e6cdb3ac856
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-25 23:02:55 +0530

    Update Command.js

--- Command.js
+++ Command.js
@@ -19,22 +19,3 @@ module.exports = {
     Module:cmd,
     commands,
 };
-command.js
-Pasindu Madushan 
-(pasindupasindu2024@gmail.com)
-General Info
-TypeJavascript
-Size486 bytes
-Modified11:43 PM Aug 8
-Created11:40 PM Aug 8
-Sharing
-Anyone with the link
-Can View
-
-prabathmdofficial
-Owner
-Description
-No description
-Download Permission
-Viewers can download
-Displaying command.js.
