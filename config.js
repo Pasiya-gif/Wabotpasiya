0dac4ec26bfea645f0d6d1cb6b044901c39a6882
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-25 23:02:15 +0530

    Update and rename Config.js to config.js

--- config.js
+++ config.js
@@ -6,27 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://.avertars.githubusercontent.com/U/106251140?v=4".
 ALIVE_MSG: process.env.ALIVE_MSG ||  "Hello, I am yourName i am alive now!".
 };
-config.js
-Pasindu Madushan 
-(pasindupasindu2024@gmail.com)
-General Info
-TypeJavascript
-Size261 bytes
-Modified11:43 PM Aug 8
-Created11:40 PM Aug 8
-Opened by me9:17 PM Aug 29
-Sharing
-Anyone with the link
-Can View
-
-prabathmdofficial
-Owner
-Description
-No description
-Download Permission
-Viewers can download
-Displaying config.js.
