c87fe5ef981d61ec3e6d801fc736308be685755a
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-25 23:16:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "qKJXlZBb#uJrIpU3HHTz0QlbDkrquUxEP18-BSIIRPaUFrjX_voY",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://.avertars.githubusercontent.com/U/106251140?v=4".
 ALIVE_MSG: process.env.ALIVE_MSG ||  "Hello, I am yourName i am alive now!".
 };
