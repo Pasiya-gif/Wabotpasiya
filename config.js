fcf8c37e01c233297cedb10dec9bf606273ed532
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-25 23:44:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "qKJXlZBb#uJrIpU3HHTz0QlbDkrquUxEP18-BSIIRPaUFrjX_voY",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://.avertars.githubusercontent.com/U/106251140?v=4".
-ALIVE_MSG: process.env.ALIVE_MSG ||  "Hello, I am yourName i am alive now!".
+ALIVE_IMG: process.env.ALIVE_IMG || "https://.avertars.githubusercontent.com/U/106251140?v=4",
+ALIVE_MSG: process.env.ALIVE_MSG ||  "Hello, I am Pasiya-MD Whatsapp Bot i am alive now!"
 };
