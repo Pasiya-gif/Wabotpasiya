7eded2ca20af4fede32ca653df3c6762b3631229
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-25 23:58:37 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,4 +1,3 @@
-
 const config = require('../config')
 const {cmd , commands} = require('../command')
 
@@ -10,7 +9,9 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+    
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+    
 }catch(e){
 console.log(e)
 reply(`${e}`)
