5de46ac469032569a3fc85af697b4923b463773d
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-26 22:36:54 +0530

    Create System.js

---
+++ plugins/System.js
@@ -0,0 +1,26 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = requrie('os') 
+cmd({
+    pattern: "system",
+    alias:["status","botinfo"],
+    "check up time , ram usage and more "
+    desc: "Check bot online or no.",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `*Uptime:*  ${runtime(process.uptime())}
+*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*HostName:* ${os.hostname()}
+*Owner:* Prabath Kumara & ᏢᎪՏᏆᎽᎪ ᏴϴᎽ
+`
+return reply('${status}')
+
+
+}cach(e){
+console.log(e)
+reply('${e}')
+}
+})
