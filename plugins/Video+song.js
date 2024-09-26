2c5f6aee2051be1f2cd07d402a7fff18d668a9e6
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-26 14:20:29 +0530

    Update Video+song.js

--- plugins/Video+song.js
+++ plugins/Video+song.js
@@ -23,15 +23,59 @@ viwes: s{data.viwes}
 MADE BY ♥️ 👉𝙥𝙖𝙨𝙞𝙮𝙖 𝙗𝙤𝙮👈 ❗
 '
 await conn.sendmessege(frome.{image:{url: data.thumbnail},caption:desc},{qouted:mek}):
-
+await conn.sendmassege(from.{document:{url:downloadurl,mimetype:"audio/mpeg",fileName:data.title + ".mp3,caption:"MADE BY ♥️ 👉𝙥𝙖𝙨𝙞𝙮𝙖 𝙗𝙤𝙮👈 ❗"},{qouted:mek})
+                             
   //download audio
 
 let down = await fg.yta(url)
 let downloadurl = down.dl_url
 
-//send audio message 
+//send audio+document message 
 await conn.sendmassege(from.{audio:{url:downloadurl,mimetype:"audio/mpeg"},{qouted:mek})
+downloadurl,mimetype:"audio/mpeg"},{qouted:mek})
 
+  
+  
+}catch(e){
+  consol.log(e)
+reply('s'{e})
+}
+})
+
+//===========vidio-dl===========
+
+cmd({
+    pattern: "video",
+    desc: "download video.",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("please give me url or title")
+const search =  await yts(q)
+const deta = search.vidios[0]:
+const url = data.url
+let desc = '
+🫵 * 𝙮𝙤𝙪𝙧 𝙫𝙞𝙙𝙚𝙤 𝙙𝙤𝙬𝙣𝙡𝙤𝙖𝙙 * ❤️ = 👉⬇️👈
+title: s{data.title}
+description s{data.description}
+time: s{data.timestamp}
+ago: s{data.ago}
+viwes: s{data.viwes}
+MADE BY ♥️ 👉𝙥𝙖𝙨𝙞𝙮𝙖 𝙗𝙤𝙮👈 ❗
+'
+await conn.sendmessege(frome.{image:{url: data.thumbnail},caption:desc},{qouted:mek}):
+
+//download video 
+
+let down = await fg.ytv(url)
+let downloadurl = down.dl_url
+
+//send video+document message 
+    
+await conn.sendmassege(from.{video:{url:downloadurl,mimetype:"video/mp4"},{qouted:mek})
+await conn.sendmassege(from.{document:{url:downloadurl,mimetype:"video/mp4",fileName:data.title + ".mp4",caption:"MADE BY ♥️ 👉𝙥𝙖𝙨𝙞𝙮𝙖 𝙗𝙤𝙮👈 ❗"},{qouted:mek})
 
   
   
