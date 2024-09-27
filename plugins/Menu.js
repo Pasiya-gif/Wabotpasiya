cace02562bb5abcb5b89bc9cb44becb153b2c680
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-27 07:48:31 +0530

    Create Menu.js

---
+++ plugins/Menu.js
@@ -0,0 +1,55 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "menu",
+    desc: "get cmd list .",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: '',
+}:
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = '👋 *🇭 🇪 🇱 🇱 🇴*'${pushname}*'
+>  *DOWNLOAD COMANDS* ⬇️
+${menu.download}
+
+> *MAIN COMMANDS* 👑
+
+
+${menu.main}
+> *GROUP COMMANDS* ⏩
+${menu.group}
+
+
+
+> *OWNER COMMANDS* 📶
+
+${menu.owner}
+> *CONVERT COMMANDS* 😓
+${menu.convert}
+> *SEARCH COMMANDS* 👀
+
+${menu.search} 
+POWERD BY 👉♥️𝙥𝙖𝙨𝙞𝙮𝙖♥️👈
+,
+await conn.sendMessage(from,{texst:madeMenu},{quote:mek})
+  
+}cach(e){
+console.log(e)
+reply('${e}')
+}
