a876263f95dd5653d23d9d44afa699a6353ca3fb
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-28 13:49:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -191,8 +191,9 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
+//============================================================================ 
 
-}
+})
 }
 app.get("/", (req, res) => {
 res.send("hey, bot started✅");
