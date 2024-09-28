5f8a0acd82270f5dc7c75665b39ce653e03457a1
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-09-28 13:58:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -193,8 +193,6 @@ command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, i
 }});
 //============================================================================ 
 
-}
-}
 app.get("/", (req, res) => {
 res.send("hey, bot started✅");
 });
