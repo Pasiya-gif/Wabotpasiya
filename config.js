34ae85d731b70f4598f426406ed7242403856594
Pasiya-gif <pasindupasindu2024@gmail.com>
2024-10-21 21:20:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "qKJXlZBb#uJrIpU3HHTz0QlbDkrquUxEP18-BSIIRPaUFrjX_voY",
+SESSION_ID: process.env.SESSION_ID || "zbBm3azb#iM0uKeyyzo8PfZPTJkz9DHAwFxFUjdmgL0y4Sf3HivI",
 MONGODB: process.env.MONGODB || "mongodb+srv://pasindupasindu:VnrhrETa6cMGbcR8@cluster0.tx04t.mongodb.net/",
 };
